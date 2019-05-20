d245654510f3bda547938429b3a3178228c01666
Luis Herrera <herrera.lrh@gmail.com>
2019-05-20 21:27:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ end
   post "/login" do
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(parmas[:password])
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
       redirect to "/account"
     else
       redirect to "/failure"
